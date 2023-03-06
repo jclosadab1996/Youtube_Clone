50f538c22ad26f8fe85b0744529586452ba2f587
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2023-03-06 20:47:17 +0900

    Primera subida de los archivos del clone de youtube

---
+++ src/utils/fetchFromAPI.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "6498189a2bmsh7c08daf4f865975p135ff7jsnfd563d092abb",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
