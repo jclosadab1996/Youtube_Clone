04d79032e052fe4d6fff9eca478a38646e3cf014
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2023-03-06 22:48:41 +0900

    Codigo de videodetail y search culminados

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
   Navbar,
   Feed,
-  VideoDetail,
-  ChannelDetail,
-  SearchFeed,
 } from "./components";
 
 const App = () => (
@@ -14,7 +14,7 @@ const App = () => (
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
+        <Route exact path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,23 @@ import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -19,8 +32,8 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton
         type="submit"
